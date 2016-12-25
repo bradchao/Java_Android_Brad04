b60bcb115a98a9b82b183b8fdbf9d2ce55717734
brad <brad@brad.tw>
2016-12-25 15:30:05 +0800

    v9

--- app/src/main/java/tw/org/iii/brad04/MyService.java
+++ app/src/main/java/tw/org/iii/brad04/MyService.java
@@ -5,7 +5,13 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
+    private Timer timer;
+    private int i;
+
     @Override
     public IBinder onBind(Intent intent) {
         //throw new UnsupportedOperationException("Not yet implemented");
@@ -15,7 +21,16 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        timer = new Timer();
+        timer.schedule(new MyTask(), 0, 1000);
         Log.v("brad", "MyService:onCreate");
+    }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            Log.v("brad", "i = " + i++);
+        }
     }
 
     @Override
@@ -27,6 +42,10 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.v("brad", "MyService:onDestroy");
+        if (timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
     }
 }
